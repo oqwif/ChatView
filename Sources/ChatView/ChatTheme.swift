c485b0bb0e01b02896cc2e383423d2fb2ac4b581
oqwif <jimconroy.pm@gmail.com>
2023-09-23 14:56:10 +1000

    Made ChatTheme init public

--- Sources/ChatView/ChatTheme.swift
+++ Sources/ChatView/ChatTheme.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 public struct ChatTheme {
-    var userMessageBackgroundColor: Color = Color(UIColor.systemGray4)
-    var characterMessageBackgroundColor: Color = Color(UIColor.systemGray6)
-    var userMessageFont: Font = .system(size: 14, weight: .medium)
-    var characterMessageFont: Font = .system(size: 14, weight: .medium)
-    var userMessageTextColor: Color = Color(UIColor.label)
-    var characterMessageTextColor: Color = Color(UIColor.label)
-    var errorMessageFont: Font = .system(size: 14, weight: .medium)
-    var retryButtonFont: Font = .system(size: 12, weight: .medium)
-    var retryButtonBackgroundColor: Color = Color(UIColor.systemGray)
-    var retryButtonTextColor: Color = Color(UIColor.white)
-    var animatedEllipsisColor: Color = Color(UIColor.systemGray2)
-    var animatedEllipsisSize: CGFloat = 6.0
+    public var userMessageBackgroundColor: Color
+    public var characterMessageBackgroundColor: Color
+    public var userMessageFont: Font
+    public var characterMessageFont: Font
+    public var userMessageTextColor: Color
+    public var characterMessageTextColor: Color
+    public var errorMessageFont: Font
+    public var retryButtonFont: Font
+    public var retryButtonBackgroundColor: Color
+    public var retryButtonTextColor: Color
+    public var animatedEllipsisColor: Color
+    public var animatedEllipsisSize: CGFloat
+
+    public init(
+        userMessageBackgroundColor: Color = Color(UIColor.systemGray4),
+        characterMessageBackgroundColor: Color = Color(UIColor.systemGray6),
+        userMessageFont: Font = .system(size: 14, weight: .medium),
+        characterMessageFont: Font = .system(size: 14, weight: .medium),
+        userMessageTextColor: Color = Color(UIColor.label),
+        characterMessageTextColor: Color = Color(UIColor.label),
+        errorMessageFont: Font = .system(size: 14, weight: .medium),
+        retryButtonFont: Font = .system(size: 12, weight: .medium),
+        retryButtonBackgroundColor: Color = Color(UIColor.systemGray),
+        retryButtonTextColor: Color = Color(UIColor.white),
+        animatedEllipsisColor: Color = Color(UIColor.systemGray2),
+        animatedEllipsisSize: CGFloat = 6.0
+    ) {
+        self.userMessageBackgroundColor = userMessageBackgroundColor
+        self.characterMessageBackgroundColor = characterMessageBackgroundColor
+        self.userMessageFont = userMessageFont
+        self.characterMessageFont = characterMessageFont
+        self.userMessageTextColor = userMessageTextColor
+        self.characterMessageTextColor = characterMessageTextColor
+        self.errorMessageFont = errorMessageFont
+        self.retryButtonFont = retryButtonFont
+        self.retryButtonBackgroundColor = retryButtonBackgroundColor
+        self.retryButtonTextColor = retryButtonTextColor
+        self.animatedEllipsisColor = animatedEllipsisColor
+        self.animatedEllipsisSize = animatedEllipsisSize
+    }
 }
