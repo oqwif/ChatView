18687ef4c81d99afcea57009ad41082f78225410
oqwif <jimconroy.pm@gmail.com>
2023-10-05 11:15:05 +1000

    Updated minimum macOS version number to v13 to support TextField auto expand

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "ChatView",
     platforms: [
         .iOS(.v16),
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
