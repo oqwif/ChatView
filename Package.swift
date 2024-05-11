fb6616b303a753423b76069fa295a3dabfbeeb76
oqwif <jimconroy.pm@gmail.com>
2024-05-11 10:57:35 +1000

    Updated package version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["ChatView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/oqwif/OpenAI.git", .branch("main")),
+        .package(url: "https://github.com/MacPaw/OpenAI.git", from: "0.2.8")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
